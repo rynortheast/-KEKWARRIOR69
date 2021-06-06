53725dbcfddbe02fac6350a2541fdcd22047b3ce
Ryo Vamirion <rynortheast@yandex.ru>
2021-06-06 03:31:39 +0300

    task__4-1

---
+++ task__4-1/src/main/java/com/rynortheast/task__4_1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rynortheast.task__4_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+}
