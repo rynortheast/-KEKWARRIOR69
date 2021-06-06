25dd0ecf0370d90cef79a33827e56e9cdff1c981
Ryo Vamirion <rynortheast@yandex.ru>
2021-06-06 04:01:44 +0300

    task__5-2

--- task__5-2/src/main/java/com/rynortheast/task__5_2/MainActivity.java
+++ task__5-2/src/main/java/com/rynortheast/task__5_2/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rynortheast.task__4_1;
+package com.rynortheast.task__5_2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_second);
     }
 }
